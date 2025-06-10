5c50bedfe11bb80c85886220bcd0e13276ce09f5
barba <sandrolini.barbara@nebbialab.it>
2025-06-10 17:54:25 +0200

    First configurator setup

---
+++ darkai/src/app/components/Dentiera.tsx
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+import {useFBX, useTexture} from "@react-three/drei";
+import {useEffect} from "react";
+
+export default function Dentiera() {
+    const fbx = useFBX('/models/Dentiera.fbx');
+    const props = useTexture({
+        map: 'textures/dentieraBase/DefaultMaterial_Base_color.webp',
+        normalMap: 'textures/dentieraBase/DefaultMaterial_Normal.webp',
+        metalnessMap: 'textures/dentieraBase/DefaultMaterial_Metallic.webp',
+        roughnessMap: 'textures/dentieraBase/DefaultMaterial_Roughness.webp',
+        aoMap: 'textures/dentieraBase/DefaultMaterial_Mixed_AO.webp',
+    });
+
+    useEffect(() => {
+        const dentieraMaterial = new THREE.MeshStandardMaterial({
+            map: props.map,
+            normalMap: props.normalMap,
+            metalnessMap: props.metalnessMap,
+            roughnessMap: props.roughnessMap,
+            aoMap: props.aoMap,
+            metalness: 0.5,
+            roughness: 0.7
+        });
+        fbx.children[0].material = dentieraMaterial;
+        console.log(fbx);
+    }, []);
+
+    return <primitive object={fbx}/>
+}
