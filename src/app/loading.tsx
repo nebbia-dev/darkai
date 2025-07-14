3f5c8811ae0a2ecb25829ec799a8f9813ad78f69
barba <sandrolini.barbara@nebbialab.it>
2025-07-14 10:58:52 +0200

    Loading screen temp fix

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
 
     return(
-        <div className="w-[100vw] h-[100vh] flex flex-col justify-center align-center">
+        <div className="w-[100vw] h-[100vh] flex flex-col justify-center align-center absolute z-30">
             <span className="loader mb-8 inline-block mx-auto"></span>
             <h2 className="text-gray-950 mx-auto">
                 Loading...
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,12 +38,12 @@ export default function Home() {
             <div className={`flex ${!ui ? 'flex-row' : 'flex-col'} w-[100vw] mx-auto bg-gray-50 relative`}>
                   {/*<button className="absolute top-4 left-4 text-white bg-black rounded-[50%] h-12 w-12 cursor-pointer font-bold z-30" onClick={setUI}>UI</button>*/}
 
-                <div className={`${!ui ? 'w-[50vw] h-[calc(100vh-54px)]' : 'w-full h-[60vh]'}`}>
+                <div className={`relative ${!ui ? 'w-[50vw] h-[calc(100vh-54px)]' : 'w-full h-[60vh]'}`}>
                     <Suspense fallback={<Loading/>}>
                           <Scene/>
                     </Suspense>
                         {loaded && <ActionBar ui={ui}/>}
-                      </div>
+                </div>
                       <div className={`${!ui ? 'w-[50vw] h-[calc(100vh-54px)]' : 'w-full h-[calc(40vh-54px)]'}`}>
                           {loaded && <Selection ui={ui}/>}
                       </div>
