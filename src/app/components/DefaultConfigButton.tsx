b97ed3377e3735cd92acd8424cb5f00ebef5d3c1
barba <sandrolini.barbara@nebbialab.it>
2025-08-07 17:26:48 +0200

    Clicking on a selected default config now deselects it

--- src/app/components/DefaultConfigButton.tsx
+++ src/app/components/DefaultConfigButton.tsx
@@ -3,16 +3,39 @@ import {State} from "@/app/types/State";
 
 export default function DefaultConfigButton({teeth, color} : {teeth: string, color: string}) {
     const setDefaultConfig = useTeethStore((state: State) => state.setDefaultConfig);
+    const setActiveDefault = useTeethStore((state: State) => state.setActiveDefault);
+    const reset = useTeethStore((state: State) => state.reset);
     const activeDefault = useTeethStore((state: State) => state.activeDefault);
     function setConfig() {
+
         switch(color) {
          case 'goldDiamond':
+             if(activeDefault === teeth + 'goldDiamond') {
+                 setActiveDefault(undefined, undefined);
+                 reset();
+                 return;
+             }
              return setDefaultConfig(teeth + 'Diamond', 'gold');
          case 'roseDiamond':
+             if(activeDefault === teeth + 'roseDiamond') {
+                 setActiveDefault(undefined, undefined);
+                 reset();
+                 return;
+             }
              return setDefaultConfig(teeth + 'Diamond', 'rose');
          case 'whiteDiamond':
+             if(activeDefault === teeth + 'whiteDiamond') {
+                 setActiveDefault(undefined, undefined);
+                 reset();
+                 return;
+             }
              return setDefaultConfig(teeth + 'Diamond', 'white');
          default:
+             if(activeDefault === teeth + color) {
+                 setActiveDefault(undefined, undefined);
+                 reset();
+                 return;
+             }
              return setDefaultConfig(teeth, color);
         }
     }
