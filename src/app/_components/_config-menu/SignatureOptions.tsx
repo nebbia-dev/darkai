cc540c4b41773e96256ad10217c095a0d61d93c9
barba <sandrolini.barbara@nebbialab.it>
2025-11-20 12:20:10 +0100

    Signature logos added

--- src/app/_components/_config-menu/SignatureOptions.tsx
+++ src/app/_components/_config-menu/SignatureOptions.tsx
@@ -13,19 +13,51 @@ export default function SignatureOptions() {
 
     return (
         <>
-            <button type="button" value="vamp" className="cursor-pointer w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center" onClick={(e) => setSignatureDesign(e.currentTarget.value)}>Vamp
+            <button type="button" value="vamp"
+                    className="cursor-pointer w-[95%] h-[120px] mx-auto mb-4"
+                    onClick={(e) => setSignatureDesign(e.currentTarget.value)}>
+                <img className="h-full object-cover object-center w-full rounded-3xl" src="/signatures-icons/vamp.svg" alt="vamp-logo"/>
             </button>
-            <button type="button" value="sprinkles" className="cursor-pointer w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center" onClick={(e) => setSignatureDesign(e.currentTarget.value)}>Sprinkles
+            <button type="button" value="sprinkles"
+                    className="cursor-pointer w-[95%] h-[120px] mx-auto mb-4"
+                    onClick={(e) => setSignatureDesign(e.currentTarget.value)}>
+                <img className="h-[120px] object-cover object-center w-full rounded-3xl" src="/signatures-icons/sprinkles.svg"
+                     alt="sprinkles-logo"/>
             </button>
-            <button type="button" value="bubblegum" className="cursor-pointer w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center" onClick={(e) => setSignatureDesign(e.currentTarget.value)}>Bubble Gum
+            <button type="button" value="bubblegum"
+                    className="cursor-pointer w-[95%] h-[120px] mx-auto mb-4"
+                    onClick={(e) => setSignatureDesign(e.currentTarget.value)}>
+                <img className="h-[120px] object-cover object-center w-full rounded-3xl"
+                     src="/signatures-icons/bubblegum.svg"
+                     alt="bubblegum-logo"/>
             </button>
-            <button type="button" value="braces" className="cursor-pointer w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center" onClick={(e) => setSignatureDesign(e.currentTarget.value)}>Braces
+            <button type="button" value="braces"
+                    className="cursor-pointer w-[95%] h-[120px] mx-auto mb-4"
+                    onClick={(e) => setSignatureDesign(e.currentTarget.value)}>
+                <img className="h-[120px] object-cover object-center w-full rounded-3xl"
+                     src="/signatures-icons/braces.svg"
+                     alt="braces-logo"/>
             </button>
-            <button type="button" value="tribal" className="cursor-pointer w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center" onClick={(e) => setSignatureDesign(e.currentTarget.value)}>Tribals
+            <button type="button" value="tribal"
+                    className="cursor-pointer w-[95%] h-[120px] mx-auto mb-4"
+                    onClick={(e) => setSignatureDesign(e.currentTarget.value)}>
+                <img className="h-[120px] object-cover object-center w-full rounded-3xl"
+                     src="/signatures-icons/tribal.svg"
+                     alt="tribals-logo"/>
             </button>
-            <button type="button" value="cross" className="cursor-pointer w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center" onClick={(e) => setSignatureDesign(e.currentTarget.value)}>Cross Spacer
+            <button type="button" value="cross"
+                    className="cursor-pointer w-[95%] h-[120px] mx-auto mb-4"
+                    onClick={(e) => setSignatureDesign(e.currentTarget.value)}>
+                <img className="h-[120px] object-cover object-center w-full rounded-3xl"
+                     src="/signatures-icons/cross.svg"
+                     alt="cross-logo"/>
             </button>
-            <button type="button" value="hammered" className="cursor-pointer w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 p-2 text-center" onClick={(e) => setSignatureDesign(e.currentTarget.value)}>Hammered Frames
+            <button type="button" value="hammered"
+                    className="cursor-pointer w-[95%] h-[120px] mx-auto"
+                    onClick={(e) => setSignatureDesign(e.currentTarget.value)}>
+                <img className="h-[120px] object-cover object-center w-full rounded-3xl"
+                     src="/signatures-icons/hammered.svg"
+                     alt="hammered-logo"/>
             </button>
         </>
     )
