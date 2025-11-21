7901fc51caefe182d10d8458dacc58259542de13
barba <sandrolini.barbara@nebbialab.it>
2025-11-21 15:40:25 +0100

    New design icons added

--- src/app/_components/_elements/_config-menu/DesignOptions.tsx
+++ src/app/_components/_elements/_config-menu/DesignOptions.tsx
@@ -1,6 +1,7 @@
 import {useTeethStore} from "@/app/_stores/teeth";
 import {State} from "@/app/_types/State";
 import checkMolar from "@/app/_helpers/_checkers/checkMolar";
+import React from "react";
 
 export default function DesignOptions({tooth}: { tooth: string|undefined }) {
     const setType =  useTeethStore((state: State) => state.setType);
@@ -14,33 +15,49 @@ export default function DesignOptions({tooth}: { tooth: string|undefined }) {
 
     return (
         <>
-            <button type="button" value="full" className={`w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 ${!checkMolar(tooth) ? 'mb-4 p-2' : ''} text-center cursor-pointer`} onClick={(e) => setDesign(e.currentTarget.value)}>Full
+            <button type="button" value="full"
+                    className={`w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 ${!checkMolar(tooth) ? 'mb-4 px-2 pb-2 pt-4' : ''} text-center cursor-pointer`}
+                    onClick={(e) => setDesign(e.currentTarget.value)}>
+                <img src="/design-icons/full.webp" alt="full-design"/>
+                <span className="text-sm">Full</span>
             </button>
-            { !checkMolar(tooth) &&
+            {!checkMolar(tooth) &&
                 <>
                     <button type="button" value="frame"
-                         className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center cursor-pointer"
-                         onClick={(e) => setDesign(e.currentTarget.value)}>Frame
+                            className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 px-2 pb-2 pt-4 text-center cursor-pointer"
+                            onClick={(e) => setDesign(e.currentTarget.value)}>
+                        <img src="/design-icons/frame.webp" alt="frame-design"/>
+                        <span className="text-sm">Frame</span>
                     </button>
-                    <button type="button" value="bar" className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center cursor-pointer" onClick={(e) => setDesign(e.currentTarget.value)}>Spacer
+                    <button type="button" value="bar"
+                            className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 px-2 pb-2 pt-4 text-center cursor-pointer"
+                            onClick={(e) => setDesign(e.currentTarget.value)}>
+                        <img src="/design-icons/spacer.webp" alt="spacer-design"/>
+                        <span className="text-sm">Spacer</span>
                     </button>
                 </>
             }
-            { (tooth === 'cidx' || tooth === 'cisx') &&
+            {(tooth === 'cidx' || tooth === 'cisx') &&
                 <button type="button" value="bigBar"
-                     className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center cursor-pointer"
-                     onClick={(e) => setDesign(e.currentTarget.value)}>Bar
+                        className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 px-2 pb-2 pt-4 text-center cursor-pointer"
+                        onClick={(e) => setDesign(e.currentTarget.value)}>
+                    <img src="/design-icons/bar.webp" alt="bar-design"/>
+                    <span className="text-sm">Bar</span>
                 </button>
             }
-            { !checkMolar(tooth) &&
+            {!checkMolar(tooth) &&
                 <>
                     <button type="button" value="bezel"
-                            className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 p-2 text-center cursor-pointer"
-                            onClick={(e) => setDesign(e.currentTarget.value)}>Bezel
+                            className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 mb-4 px-2 pb-2 pt-4 text-center cursor-pointer"
+                            onClick={(e) => setDesign(e.currentTarget.value)}>
+                        <img src="/design-icons/bezel.webp" alt="bezel-design"/>
+                        <span className="text-sm">Bezel</span>
                     </button>
                     <button type="button" value="enamel"
-                            className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 p-2 text-center cursor-pointer"
-                            onClick={(e) => setDesign(e.currentTarget.value)}>Enamel
+                            className="w-[95%] h-[120px] mx-auto rounded-3xl bg-stone-200 px-2 pb-2 pt-4 text-center cursor-pointer"
+                            onClick={(e) => setDesign(e.currentTarget.value)}>
+                        <img src="/design-icons/enamel.webp" alt="enamel-design"/>
+                        <span className="text-sm">Enamel</span>
                     </button>
                 </>
             }
