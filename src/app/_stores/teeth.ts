dd563518ac72f65539e3e8d824a5bcc7a9b7a7e7
barba <sandrolini.barbara@nebbialab.it>
2025-11-21 18:06:35 +0100

    Selected config options now have a border

--- src/app/_stores/teeth.ts
+++ src/app/_stores/teeth.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import {produce} from "immer";
-import {Pave, State, Stone} from "@/app/_types/State";
+import {State} from "@/app/_types/State";
 import firstCapital from "@/app/_helpers/_string-modders/firstCapital";
 import json from "@/utils/prices.json";
 import {createClient} from "@/utils/supabase/client";
