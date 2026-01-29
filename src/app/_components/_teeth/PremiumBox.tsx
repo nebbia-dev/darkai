0f7a98a3601d466363af3d8bc6aa68ac2b36ba3a
barba <sandrolini.barbara@nebbialab.it>
2026-01-29 16:04:20 +0100

    New icons + new 3D text on the packaging

--- src/app/_components/_teeth/PremiumBox.tsx
+++ src/app/_components/_teeth/PremiumBox.tsx
@@ -1,5 +1,5 @@
-import React, {Ref, useMemo} from "react";
-import {Text3D, useGLTF, useTexture} from "@react-three/drei";
+import React, {Ref, useEffect, useMemo, useRef} from "react";
+import {Center, Text3D, useGLTF, useTexture} from "@react-three/drei";
 import {useTeethStore} from "@/app/_stores/teeth";
 import * as THREE from 'three';
 import {State} from "@/app/_types/State";
@@ -13,6 +13,10 @@ import BoxMaterialSup from "@/app/_components/_materials/BoxMaterialSup";
 import BoxMaterialInf from "@/app/_components/_materials/BoxMaterialInf";
 
 export default function PremiumBox({ref} : {ref:Ref<Group|null>}) {
+
+    const textRef = useRef(null);
+    const customText = useTeethStore((state: State) => state.packaging.text);
+
     const premiumBox = useMemo(() => {
         return useGLTF('/models/Packaging.glb')
     }, []);
@@ -21,6 +25,12 @@ export default function PremiumBox({ref} : {ref:Ref<Group|null>}) {
     lightDisks.flipY = false;
     const packaging = useTeethStore((state: State) => state.packaging);
 
+    useEffect(() => {
+        if(textRef.current) {
+            (textRef.current as THREE.Mesh).geometry.center();
+        }
+    }, [customText])
+
     return(
         <>
             <group ref={ref}
@@ -28,11 +38,12 @@ export default function PremiumBox({ref} : {ref:Ref<Group|null>}) {
                    // position={[-20.060500000193823, 0, -9.505650000058095]} per l'animazione
             >
                 <group rotation={[Math.PI / 2, Math.PI, Math.PI / 2]} scale={[3.5, 3.5, 3.5]}>
-
-                    {/*<Text3D font="/Inter_Bold.json">*/}
-                    {/*    Hello world!*/}
-                    {/*    <meshNormalMaterial />*/}
-                    {/*</Text3D>*/}
+                    <Center rotation={[0, 0.91, 1.58]} position={[-0.02, 0, -0.07]} visible={customText !== ''}>
+                        <Text3D font="/Inter_Bold.json" size={0.1} ref={textRef}>
+                            {customText}
+                            <FullMaterial color={packaging.details} finish="polished"/>
+                        </Text3D>
+                    </Center>
 
                     {/*TOP*/}
                     {/*Sportello*/}
